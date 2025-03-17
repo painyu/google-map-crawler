bd0162ffc848d60ff2c66de794e8a2391f24958d
jack.yeungy <jack.yeungy@finpoints.com>
2025-03-17 10:07:11 +0800

    提交嗲吗

--- src/google_map_pick/entities/google_map_pick.entity.ts
+++ src/google_map_pick/entities/google_map_pick.entity.ts
@@ -5,38 +5,49 @@ export class GoogleMapPick {
   @PrimaryColumn()
   @PrimaryGeneratedColumn()
   id: number;
-  @Column()
-  name: string;
-  @Column()
-  job_id: string;
+
   @Column()
   google_id: string;
+
   @Column()
-  address_details: string;
-  @Column()
-  address: string;
+  google_name: string;
+
   @Column()
   phone: string;
+
   @Column()
-  phone_details: string;
+  emails:string;
+
   @Column()
   labels: string;
+
   @Column()
   website: string;
+
   @Column()
   country: string;
+
   @Column()
   province: string;
+
   @Column()
   city: string;
+
   @Column()
   region: string;
+
   @Column()
   street: string;
+
+  @Column()
+  address: string;
+
+  @Column()
+  address_details: string;
+
   @Column()
   postCode: string;
+
   @Column()
-  google_image: string;
-  @Column()
-  reques_param: string;
+  job_id: string;
 }
